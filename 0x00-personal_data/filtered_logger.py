d3d8fdf9641116e19dd3cacd33940b652ae6d4ec
leroysb <leroybuliro@gmail.com>
2023-08-05 11:46:28 +0300

    task2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -27,9 +27,19 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """Format method to filter values in incoming log records using filter_datum."""
+        """Filters values in incoming log records using filter_datum
+        Args:
+            record (logging.LogRecord): Record to be formatted
+        Returns:
+            str: Formatted log record.
+        """
         message = super().format(record)
-        return filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
+        return filter_datum(
+            self.fields,
+            self.REDACTION,
+            message,
+            self.SEPARATOR
+        )
 
 
 # def get_logger() -> logging.Logger:
