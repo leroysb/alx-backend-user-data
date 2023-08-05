ccb1ec92e8b7152e7bbfbea5ac73ce90a2c0d5ad
leroysb <leroybuliro@gmail.com>
2023-08-05 13:53:05 +0300

    Encrypting passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,9 @@ def hash_password(password: str) -> bytes:
     """ returns a salted, hashed password, which is a byte string.
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hash_password: bytes, password: str) -> bool:
+    """ expects 2 arguments and returns a boolean.
+    """
+    return bcrypt.checkpw(password.encode(), hash_password)
