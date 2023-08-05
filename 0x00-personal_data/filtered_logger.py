b46b5fe61e18e51bc979b31ecf02f03cb74bc101
leroysb <leroybuliro@gmail.com>
2023-08-05 12:17:16 +0300

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -68,9 +68,8 @@ def get_db():
     password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
     host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
     db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-    
+
     try:
-        import mysql.connector
         connector = mysql.connector.connect(
             user=username,
             password=password,
