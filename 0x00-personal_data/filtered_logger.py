1eea1fd53e6712eafc6b7d50d783a3e247c553b7
leroysb <leroybuliro@gmail.com>
2023-08-05 11:41:29 +0300

    task2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """ Regex-ing """
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -11,3 +12,34 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(field + "=.*?" + separator,
                          field + "=" + redaction + separator, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format method to filter values in incoming log records using filter_datum."""
+        message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
+
+
+# def get_logger() -> logging.Logger:
+#     """ returns a logging.Logger object """
+#     logger = logging.getLogger('user_data')
+#     logger.setLevel(logging.INFO)
+#     logger.propagate = False
+#     handler = logging.StreamHandler()
+#     formatter = logging.Formatter(
+#         '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+#     handler.setFormatter(formatter)
+#     logger.addHandler(handler)
+#     return logger
