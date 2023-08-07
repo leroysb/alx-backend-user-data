3c8479732e8b81eafcfd0a6de60af32bbd752450
leroysb <leroybuliro@gmail.com>
2023-08-07 17:06:13 +0300

    Basic - Overload current_user - and BOOM!

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
-""" Module of API authentication
+""" Module of API authentication that impliments a Basic Authentication
 """
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
 from models.user import User
 import base64
+from api.v1.views import users
 
 
 class BasicAuth(Auth):
-    """ BasicAuth class
+    """ BasicAuth class that inherits from Auth
     """
     def extract_base64_authorization_header(
      self, authorization_header: str) -> str:
-        """ Extract base64 authorization header
+        """ Returns the Base64 part of the Authorization header
         """
         if authorization_header is None or \
                 type(authorization_header) is not str or \
@@ -23,7 +24,7 @@ class BasicAuth(Auth):
 
     def decode_base64_authorization_header(
      self, base64_authorization_header: str) -> str:
-        """ Decode base64 authorization header
+        """ Returns the decoded value of a Base64 string
         """
         if base64_authorization_header is None or \
                 type(base64_authorization_header) is not str:
@@ -36,7 +37,7 @@ class BasicAuth(Auth):
 
     def extract_user_credentials(
      self, decoded_base64_authorization_header: str) -> (str, str):
-        """ Extract user credentials
+        """ Returns the user email and password from the Base64 decoded value
         """
         if decoded_base64_authorization_header is None or \
                 type(decoded_base64_authorization_header) is not str or \
@@ -46,7 +47,7 @@ class BasicAuth(Auth):
 
     def user_object_from_credentials(
      self, user_email: str, user_pwd: str) -> TypeVar('User'):
-        """ User object from credentials
+        """ Returns the User instance based on his email and password
         """
         if user_email is None or type(user_email) is not str or \
                 user_pwd is None or type(user_pwd) is not str:
@@ -59,3 +60,15 @@ class BasicAuth(Auth):
             if user.is_valid_password(user_pwd):
                 return user
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Overloads Auth and retrieves the User instance for a request
+        """
+        auth_header = self.authorization_header(request)
+        base64_auth_header = self.extract_base64_authorization_header(
+         auth_header)
+        decode_base64_auth_header = self.decode_base64_authorization_header(
+         base64_auth_header)
+        user_email, user_pwd = self.extract_user_credentials(
+         decode_base64_auth_header)
+        return self.user_object_from_credentials(user_email, user_pwd)
