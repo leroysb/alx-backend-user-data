58068b431f9b061a2b046f4d877beb686d02a320
leroysb <leroybuliro@gmail.com>
2023-08-07 14:48:21 +0300

    Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -20,3 +20,26 @@ class BasicAuth(Auth):
                 not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+     self, base64_authorization_header: str) -> str:
+        """ Decode base64 authorization header
+        """
+        if base64_authorization_header is None or \
+                type(base64_authorization_header) is not str:
+            return None
+        try:
+            return base64.b64decode(
+             base64_authorization_header).decode('utf-8')
+        except Exception:
+            return None
+
+    def extract_user_credentials(
+     self, decoded_base64_authorization_header: str) -> (str, str):
+        """ Extract user credentials
+        """
+        if decoded_base64_authorization_header is None or \
+                type(decoded_base64_authorization_header) is not str or \
+                ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        return tuple(decoded_base64_authorization_header.split(':', 1))
