cf84f213421b3c95e4c5189ecbab8aae619dbbd3
leroysb <leroybuliro@gmail.com>
2023-08-10 19:38:45 +0300

    Empty session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -16,10 +16,13 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth_type = getenv("AUTH_TYPE")
 
+if auth_type == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 if auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif auth_type == 'auth':
+if auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
