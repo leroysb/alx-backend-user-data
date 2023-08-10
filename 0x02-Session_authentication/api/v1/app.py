2d59348223ca98666cfb5337f38340a922f68055
leroysb <leroybuliro@gmail.com>
2023-08-10 20:17:56 +0300

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -36,7 +36,8 @@ def before_request() -> str:
     #
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     #
     request.current_user = auth.current_user(request)
     if request.path not in excluded_paths:
@@ -46,6 +47,9 @@ def before_request() -> str:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
+        if auth.authorization_header(request) is None or \
+           auth.session_cookie(request) is None:
+            abort(401)
 
 
 @app.errorhandler(404)
