ebe7ede2f0f8492dc0242c63d00dc8e164b616de
leroysb <leroybuliro@gmail.com>
2023-08-16 18:07:45 +0300

    Find user by session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,12 +56,13 @@ class Auth:
             return None
 
     def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
-        """ get user from session id """
-        if session_id:
-            user = self._db.find_user_by(session_id=session_id)
-            if user:
-                return user
-        return None
+        """ get user from session id
+        """
+        if not session_id:
+            return None
+        user = self._db.find_user_by(session_id=session_id)
+        if user:
+            return user
 
     def destroy_session(self, user_id: int) -> None:
         """ destroy session """
