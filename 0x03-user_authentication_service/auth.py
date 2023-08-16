898d7577ec186174bd306988518b1cf59ebdc031
leroysb <leroybuliro@gmail.com>
2023-08-16 18:42:24 +0300

    Generate reset password toke

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,13 +73,13 @@ class Auth:
 
     def get_reset_password_token(self, email: str) -> str:
         """ reset password """
-        user = self._db.find_user_by(email=email)
-        if user:
-            reset_token = _generate_uuid()
-            self._db.update_user(user.id, reset_token=reset_token)
-            return reset_token
-        else:
+        try:
+            user = self._db.find_user_by(email=email)
+        except Exception:
             raise ValueError
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
 
     def update_password(self, reset_token: str, password: str) -> None:
         """ update password """
