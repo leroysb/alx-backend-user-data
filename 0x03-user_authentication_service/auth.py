cbc44305cff47cd31f90fb9831a0edb35be4ba3c
leroysb <leroybuliro@gmail.com>
2023-08-16 18:47:56 +0300

    Update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -22,6 +22,8 @@ class Auth:
     """
 
     def __init__(self):
+        """ initialize
+        """
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
@@ -68,11 +70,13 @@ class Auth:
         return user
 
     def destroy_session(self, user_id: int) -> None:
-        """ destroy session """
+        """ destroy session
+        """
         self._db.update_user(user_id, session_id=None)
 
     def get_reset_password_token(self, email: str) -> str:
-        """ reset password """
+        """ reset password
+        """
         try:
             user = self._db.find_user_by(email=email)
         except Exception:
@@ -82,11 +86,12 @@ class Auth:
         return reset_token
 
     def update_password(self, reset_token: str, password: str) -> None:
-        """ update password """
-        user = self._db.find_user_by(reset_token=reset_token)
-        if user:
-            self._db.update_user(user.id,
-                                 hashed_password=_hash_password(password),
-                                 reset_token=None)
-        else:
+        """ update password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except Exception:
             raise ValueError
+        self._db.update_user(user.id,
+                             hashed_password=_hash_password(password),
+                             reset_token=None)
