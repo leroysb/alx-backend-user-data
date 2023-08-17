e1a17b645e535973dbeb4841d943c6283d15dab5
leroysb <leroybuliro@gmail.com>
2023-08-17 18:26:49 +0300

    Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,11 +14,7 @@ def hash_password(password: str) -> bytes:
 def is_valid(hash_password: bytes, password: str) -> bool:
     """ Checks if a password is valid
     """
-    if not hash_password or not password:
-        return False
-    if not isinstance(hash_password, bytes) or not isinstance(password, str):
-        return False
     try:
         return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
-    except ValueError:
+    except Exception:
         return False
