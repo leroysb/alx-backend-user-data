784ea1bc4cadb312115c663f225315b88ac43abe
leroysb <leroybuliro@gmail.com>
2023-08-17 18:18:54 +0300

    Read and filter data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,8 @@ from typing import List
 import logging
 import mysql.connector
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
@@ -41,9 +43,6 @@ class RedactingFormatter(logging.Formatter):
             message,
             self.SEPARATOR
         )
-
-
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def get_logger() -> logging.Logger:
